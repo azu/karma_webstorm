8eb0d288e15d3fb98b7c05b31e4023ff185f8d78
azu <azuciao@gmail.com>
2013-07-23 14:39:16 +0900

    reinit

--- karma.conf.js
+++ karma.conf.js
@@ -1,74 +1,72 @@
 // Karma configuration
-// Generated on Fri May 31 2013 21:57:25 GMT+0900 (JST)
+// Generated on Tue Jul 23 2013 14:38:50 GMT+0900 (JST)
+
+module.exports = function(karma) {
+  karma.configure({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    // frameworks to use
+    frameworks: ['jasmine'],
 
 
-// frameworks to use
-frameworks = ['jasmine'];
+    // list of files / patterns to load in the browser
+    files: [
+      'test/*.js'
+    ],
 
 
-// list of files / patterns to load in the browser
-files = [
-  'test/*'
-];
+    // list of files to exclude
+    exclude: [
+      
+    ],
 
 
-// list of files to exclude
-exclude = [
-  
-];
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-reporters = ['progress'];
+    // web server port
+    port: 9876,
 
 
-// web server port
-port = 9876;
+    // cli runner port
+    runnerPort: 9100,
 
 
-// cli runner port
-runnerPort = 9100;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // level of logging
+    // possible values: karma.LOG_DISABLE || karma.LOG_ERROR || karma.LOG_WARN || karma.LOG_INFO || karma.LOG_DEBUG
+    logLevel: karma.LOG_INFO,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
-
-
-// plugins to load
-plugins = [
-  'karma-jasmine'
-];
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
